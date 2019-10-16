a5efbcc42408ed28e2cbe323929235bcfdabce03
Waltero Estoplero <waltser@web.de>
2019-10-16 14:06:15 +0200

    My first commit

---
+++ FHL-Leer-Test-001/Class5.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class Class1
+{
+	public static void Main()
+	{
+        Console.WriteLine("Nun kommt ein Formular:");
+
+        Console.ReadLine();
+	}
+}
+
+
+/*
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+public class Program
+{
+
+    public static void Main()
+    {
+        Console.WriteLine("Nun kommt ein Formular:");
+
+        Form ersteForm = new Form();
+        Application.Run(ersteForm);
+    }
+}
+*/
